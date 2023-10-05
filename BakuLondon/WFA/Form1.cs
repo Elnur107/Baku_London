2033254c4c6cfb276eea600f97e5d88d1c509a95
Elnur107 <126491959+Elnur107@users.noreply.github.com>
2023-10-05 08:09:11 +0400

    Add files via upload

---
+++ BakuLondon/WFA/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetBackgroundAndTime(Properties.Resources.baku, "Azerbaijan Standard Time");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetBackgroundAndTime(Properties.Resources.london, "GMT Standard Time");
+        }
+
+        private void SetBackgroundAndTime(System.Drawing.Image backgroundImage, string timeZoneId)
+        {
+            BackgroundImage = backgroundImage;
+            TimeZoneInfo selectedTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            label1.Text = GetTimeForTimeZone(selectedTimeZone);
+        }
+
+        private string GetTimeForTimeZone(TimeZoneInfo timeZone)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZone).ToString("HH:mm");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
